cd286126e941c669b670cbbf48ade9c8a349b585
rutu12345678 <69729788+rutu12345678@users.noreply.github.com>
2020-08-15 23:31:28 +0530

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class assign1
+{
+    public static void main(String[] args){
+       int a=70;
+       int b=20;
+       int c=10;
+       int taxper=18;
+       int prod=a+b+c;
+       int taxAmount=(taxper/100)*prod;
+       System.out.println( "Total Tx Amount"+taxAmount);
+   }
+   }    
+        
+        
