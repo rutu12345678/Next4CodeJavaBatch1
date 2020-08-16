2af87abf9b536872111cb07d5b77f9055f19a1c6
rutu12345678 <69729788+rutu12345678@users.noreply.github.com>
2020-08-16 13:55:33 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class assign2{
+    public static void main(String [ ] args) {
+    Scanner in = new Scanner (System.in);
+    {
+    double kilo = 0;
+       int height=0;
+System.out.println("Enter the weight in pound:");
+int pound=in.nextInt();
+pound = (int)(kilo/0.453592);
+System.out.println("Enter the height in feet and height:");
+int feet=in.nextInt();
+float inch=in.nextInt();
+int cm=(int) (((height*12)+inch)*2.54);
+System.out.println("weight in pound="+pound);
+System.out.println("weight in pound="+cm);
+
+
+    }
+    }
